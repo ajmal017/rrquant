974ae3810f8380b738cc4dfc559a5796c879b049
RRQuanticaAnalytics <25837756+RRQuanticaAnalytics@users.noreply.github.com>
2020-03-02 19:51:20 +1000

    Commit 2 - 2/3/2020

--- rrquant/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# Inside this __init__.py file put in the code that imports
-# the classes/functions from the other files in this package.
-# E.g. from rrfinance import function/class
-# When calling the package in another project:
-# import RRQuant OR from RRQuant import rrfinance
-
-from rrquant import rrfinance
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+
 def readme():
     with open('README.md') as f:
         return f.read()
@@ -7,21 +8,21 @@ setup(name='rrquant',
         version='0.1',
         description='A package containing classes and functions made for financial and big data analysis.',
         long_description = readme(),
-        classifiers = [
-
-        ],
         url='https://github.com/RRQuanticaAnalytics/rrquant',
         author='RRQuanticaAnalytics',
         author_email='rohan@rrquanticacapital.com',
         license='MIT',
+        packages=['rrquant'],
         install_requires = [
                 'pandas',
                 'numpy',
                 'datetime',
                 'markdown',
         ],
-        packages=['rrquant'],
+        #test_suite = 'nose.collector',
+        #tests_require = ['nose'],
+        #entry_points = {
+        #'console_scripts':['rrquant=rrquant.command_line:main'],
+        #},
         include_package_data = True,
-        zip_safe=False,
-        test_suite = 'nose.collector',
-        tests_require = ['nose'])
+        zip_safe=False,)
