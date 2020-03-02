d40674c4b4ac723c94e87c19c1547c66a6210082
RR <25837756+RRQuanticaAnalytics@users.noreply.github.com>
2020-03-02 16:40:39 +1000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='rrquant',
+        version='0.1',
+        description='A package containing classes and functions made for financial and big data analysis',
+        url='http://github.com/',
+        author='RR Quantica Capital Management',
+        author_email='rohan@rrquanticacapital.com',
+        license='MIT',
+        packages=['rrquant'],
+        zip_safe=False)	
