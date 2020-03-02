0b84c828a5b824c924c754dffe5c3543985e1174
RRQuanticaAnalytics <25837756+RRQuanticaAnalytics@users.noreply.github.com>
2020-03-02 17:25:10 +1000

    Commit 2/3/2020
    - Added MANIFEST.in file
    - Added test subdirectory with test package
    - Included test_suite and tests_require args to setup.py
    - installed nose package for testing

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='rrquant',
         version='0.1',
-        description='A package containing classes and functions made for financial and big data analysis',
+        description='A package containing classes and functions made for financial and big data analysis.',
+        long_description = readme(),
+        classifiers = [
+
+        ],
         url='https://github.com/RRQuanticaAnalytics/rrquant',
         author='RRQuanticaAnalytics',
         author_email='rohan@rrquanticacapital.com',
@@ -10,6 +18,10 @@ setup(name='rrquant',
                 'pandas',
                 'numpy',
                 'datetime',
+                'markdown',
         ],
         packages=['rrquant'],
-        zip_safe=False)	
+        include_package_data = True,
+        zip_safe=False,
+        test_suite = 'nose.collector',
+        tests_require = ['nose'])
